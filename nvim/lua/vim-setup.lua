f12990bde4a66439cf9da9d0c6de3b31e67eec9f
Dat Do Duc <doducdat2904@gmail.com>
2024-02-26 20:23:05 +0700

    Updated neovim from nvchad to lazyvim

--- nvim/lua/vim-setup.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = " "
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
-vim.cmd("highlight clear Cursorline")
-vim.cmd("highlight Cursorline gui=underline cterm=underline")
-vim.cmd("set cursorline")
